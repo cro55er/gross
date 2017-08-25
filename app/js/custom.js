203b7caf3a6f01380aaecd20b586bb91687861cf
Dmitry Bondar <dmitry.bndar@gmail.com>
2017-08-25 18:02:45 +0300

    changes

--- app/js/custom.js
+++ app/js/custom.js
@@ -8,4 +8,33 @@
         menu.classList.toggle('show');
     });
 
+    var btnsShowPopup   = document.querySelectorAll('.show-popup'),
+        overlay         = document.querySelector('.page-overlay'),
+        btnsClosePopup  = document.querySelectorAll('.popup-btn-close'),
+        popups          = document.querySelectorAll('.popup');
+
+    btnsShowPopup.forEach(function (btn) {
+        btn.addEventListener('click', function () {
+            event.preventDefault();
+            var modal = document.querySelector('.' + btn.getAttribute('data-target'));
+            modal.classList.add('show');
+            overlay.classList.add('show');
+        });
+    });
+
+    btnsClosePopup.forEach(function (btn) {
+        btn.addEventListener('click', closePopup);
+    });
+
+    overlay.addEventListener('click', closePopup);
+
+    function closePopup() {
+        event.preventDefault()
+        popups.forEach(function (popup) {
+            popup.classList.remove('show');
+        });
+        overlay.classList.remove('show');
+    }
+
+
 })();
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
-var gulp        = require('gulp'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync').create(),
-    concatCss = require('gulp-concat-css');
+var gulp         = require('gulp'),
+    sass         = require('gulp-sass'),
+    browserSync  = require('browser-sync').create(),
+    concatCss    = require('gulp-concat-css'),
+    postCss      = require('gulp-postcss'),
+    imagemin     = require('gulp-imagemin'),
+    uglify       = require('gulp-uglifyjs'),
+    autoprefixer = require('autoprefixer'),
+    clean        = require('gulp-clean'),
+    rename       = require('gulp-rename'),
+    htmlreplace  = require('gulp-html-replace'),
+    cssnano      = require('cssnano');
 
 var urls = {
     baseDir: "app",
@@ -9,6 +17,42 @@ var urls = {
     css: "app/css/*.css",
     sass: "app/sass/**/*.scss"
 }
+
+gulp.task('clean', function () {
+    return gulp.src('dist/', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('build', ['clean', 'css'], function() {
+    var processors = [
+        autoprefixer({browsers: ['last 2 versions']}),
+        cssnano(),
+    ];
+
+    gulp.src('app/fonts/**/*.*')
+        .pipe(gulp.dest('dist/fonts'));
+
+    gulp.src(urls.html)
+        .pipe(htmlreplace({
+            'css': 'css/styles.min.css',
+            'js' : 'js/scripts.min.js'
+        }))
+        .pipe(gulp.dest('dist/'));
+
+    gulp.src('app/img/*.*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+
+    gulp.src('app/js/*.js')
+        .pipe(uglify())
+        .pipe(rename('scripts.min.js'))
+        .pipe(gulp.dest('dist/js/'))
+
+    gulp.src('app/css/main.css')
+        .pipe(postCss(processors))
+        .pipe(rename('styles.min.css'))
+        .pipe(gulp.dest('dist/css/'));
+});
 
 gulp.task('sass', function () {
   return gulp.src(urls.sass)
